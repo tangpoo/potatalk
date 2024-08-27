6c198e60d87c9d4015d7a2080a5709dd19208b6e
tangpoo <lhinav3@gmail.com>
2024-08-28 01:09:03 +0900

    #2 Test: Member-Service 회원가입 API Test 구현

--- member-service/src/main/java/com/potatalk/memberservice/dto/MemberRes.java
+++ member-service/src/main/java/com/potatalk/memberservice/dto/MemberRes.java
@@ -3,10 +3,12 @@ package com.potatalk.memberservice.dto;
 import com.potatalk.memberservice.domain.Member;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @Getter
 @AllArgsConstructor
+@Slf4j
 public class MemberRes {
 
     private String username;
