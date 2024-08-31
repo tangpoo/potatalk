5bb98431b29c1dd412ea392996e55141ebdb7ff4
tangpoo <lhinav3@gmail.com>
2024-08-31 13:38:13 +0900

    #15 Build: Member-Service update, delete API 구현

--- member-service/src/main/java/com/potatalk/memberservice/domain/Member.java
+++ member-service/src/main/java/com/potatalk/memberservice/domain/Member.java
@@ -1,5 +1,7 @@
 package com.potatalk.memberservice.domain;
 
+import com.potatalk.memberservice.dto.MemberRes;
+import com.potatalk.memberservice.dto.MemberUpdateDto;
 import com.potatalk.memberservice.dto.SignInDto;
 import com.potatalk.memberservice.dto.SingUpDto;
 import lombok.Getter;
@@ -9,6 +11,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import reactor.core.publisher.Mono;
 
 @Table("members")
 @Getter
@@ -41,5 +44,11 @@ public class Member {
     public boolean passwordMatch(final String password,
         final PasswordEncoder passwordEncoder) {
         return passwordEncoder.matches(password, this.password);
+    }
+
+    public Mono<Member> update(final MemberUpdateDto memberUpdateDto) {
+        this.username = memberUpdateDto.getUsername();
+        this.nickName = memberUpdateDto.getNickName();
+        return Mono.just(this);
     }
 }
