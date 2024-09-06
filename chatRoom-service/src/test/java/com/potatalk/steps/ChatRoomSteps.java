68915edd724f4a32f98443f44ae04ee024e32eb0
tangpoo <131866367+tangpoo@users.noreply.github.com>
2024-09-07 00:27:11 +0900

    Feat/chatroom ChatRoom-Service 채팅방 입장 (#33)
    * #9 Test: MemberRepositoryTests 제거
    
    * #3 Build: Member-Service Login API 및 테스트 구현
    
    * #24 Build: ChatRoom-Service 채팅방 입장 API 구현
    
    * #24 Test: ChatRoom-Service 채팅방 입장 Test

--- chatRoom-service/src/test/java/com/potatalk/steps/ChatRoomSteps.java
+++ chatRoom-service/src/test/java/com/potatalk/steps/ChatRoomSteps.java
@@ -5,15 +5,23 @@ import com.potatalk.chatroomservice.domain.ChatRoomStatus;
 import com.potatalk.chatroomservice.dto.CreateChatRoomDto;
 
 public class ChatRoomSteps {
-    public static CreateChatRoomDto createGroupChatRoomDto() {
-        return new CreateChatRoomDto(1L, null, "chatRoomName-1234", false, "secretKey-1234", 3);
+    public static CreateChatRoomDto createGroupChatRoomDto(Boolean isPrivate, String secretKey) {
+        return new CreateChatRoomDto(1L, null, "chatRoomName-1234", isPrivate, secretKey, 3);
     }
 
     public static CreateChatRoomDto createOneToOneChatRoomDto() {
         return new CreateChatRoomDto(1L, 2L, "chatRoomName-1234", false, "secretKey-1234", 2);
     }
 
-    public static ChatRoom creatChatRoom(CreateChatRoomDto createChatRoomDto, ChatRoomStatus chatRoomStatus) {
+    public static ChatRoom createChatRoom(CreateChatRoomDto createChatRoomDto, ChatRoomStatus chatRoomStatus) {
         return ChatRoom.create(createChatRoomDto, chatRoomStatus);
+    }
+
+    public static ChatRoom createChatRoom() {
+        return ChatRoom.create(createGroupChatRoomDto(false, null), ChatRoomStatus.GROUP);
+    }
+
+    public static ChatRoom createPrivateChatRoom() {
+        return ChatRoom.create(createGroupChatRoomDto(true, "secretKey-1234"), ChatRoomStatus.GROUP);
     }
 }
