48cde67c096ff5b761d9a9687450767dd4921dde
tangpoo <lhinav3@gmail.com>
2024-09-12 01:36:41 +0900

    #30 Build: Chat-Service WebSocket + STOMP Config 클래스

--- chat-service/src/main/java/com/potatalk/config/StompHandler.java
+++ chat-service/src/main/java/com/potatalk/config/StompHandler.java
@@ -1,10 +1,25 @@
 package com.potatalk.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class StompHandler {
+public class StompHandler implements ChannelInterceptor {
 
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+        if (StompCommand.CONNECT == accessor.getCommand()) {
+            // todo 클라이언트 검증 로직
+        }
+
+        return ChannelInterceptor.super.preSend(message, channel);
+    }
 }
