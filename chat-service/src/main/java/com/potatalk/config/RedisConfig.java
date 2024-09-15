6eaf530c4e3b9f70fa218e0c882facdb74745de2
tangpoo <lhinav3@gmail.com>
2024-09-15 16:16:36 +0900

    #30 Refactor: RedisConfig messageListenerAdapter 메서드 명 추가, yml 파일 추가

--- chat-service/src/main/java/com/potatalk/config/RedisConfig.java
+++ chat-service/src/main/java/com/potatalk/config/RedisConfig.java
@@ -16,10 +16,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int redisPort;
 
     @Bean
@@ -47,6 +47,6 @@ public class RedisConfig {
 
     @Bean
     public MessageListenerAdapter messageListenerAdapter(ChatSubscriber subscriber) {
-        return new MessageListenerAdapter(subscriber);
+        return new MessageListenerAdapter(subscriber, "sendMessage");
     }
 }
