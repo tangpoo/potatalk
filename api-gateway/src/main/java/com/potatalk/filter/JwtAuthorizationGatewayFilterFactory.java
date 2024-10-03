26a53898439e2177663426dfdb093d12357585cf
tangpoo <lhinav3@gmail.com>
2024-10-03 16:28:01 +0900

    #51 Refactor: API-Gateway JWT 누락 시 UNAUTHORIZED 에러 처리

--- api-gateway/src/main/java/com/potatalk/filter/JwtAuthorizationGatewayFilterFactory.java
+++ api-gateway/src/main/java/com/potatalk/filter/JwtAuthorizationGatewayFilterFactory.java
@@ -32,38 +32,45 @@ public class JwtAuthorizationGatewayFilterFactory extends
             String path = request.getURI().getPath();
             log.info("in filter");
 
+            // 로그인과 회원가입 경로는 필터를 통과
             if (path.equals("/api/v1/members/signup") || path.equals("/api/v1/members/signin")) {
                 log.info("pass filter");
                 return chain.filter(exchange);
             }
 
+            // 헤더에서 토큰 추출
             String tokenValue = jwtUtil.getTokenFromHeader(request);
 
-            log.info("in filter: " + tokenValue);
+            log.info("in filter with token: " + tokenValue);
 
-            if (StringUtils.hasText(tokenValue)) {
-                try {
-                    Claims payload = jwtUtil.getUserInfoFromToken(tokenValue);
-
-                    String username = payload.getSubject();
-//                    String username = payload.get("username", String.class);
-
-                    ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
-//                        .header("X-User-Email", email)
-                        .header("X-Username", username)
-                        .build();
-
-                    log.info(String.format("User %s joined the server", username));
-
-                    return chain.filter(exchange.mutate().request(mutatedRequest).build());
-                } catch (Exception e) {
-                    log.error("JWT 검증 실패: {}", e.getMessage());
-                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                    return exchange.getResponse().setComplete();
-                }
+            // 토큰이 없을 경우 Unauthorized 응답 설정
+            if (!StringUtils.hasText(tokenValue)) {
+                log.error("JWT 토큰이 존재하지 않음");
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
             }
 
-            return chain.filter(exchange);
+            // 토큰이 있을 경우 검증
+            try {
+                Claims payload = jwtUtil.getUserInfoFromToken(tokenValue);
+
+                String username = payload.getSubject();
+
+                // 토큰의 정보를 요청 헤더에 추가
+                ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
+                    .header("X-Username", username)
+                    .build();
+
+                log.info(String.format("User %s joined the server", username));
+
+                // 수정된 요청으로 필터 체인 통과
+                return chain.filter(exchange.mutate().request(mutatedRequest).build());
+            } catch (Exception e) {
+                // 토큰 검증 실패 시 Unauthorized 응답 설정
+                log.error("JWT 검증 실패: {}", e.getMessage());
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
         };
     }
 
