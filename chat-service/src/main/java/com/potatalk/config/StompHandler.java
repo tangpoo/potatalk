7e06acec392d3272b27ff9635ca1be33a277adee
tangpoo <lhinav3@gmail.com>
2025-04-16 23:00:07 +0900

    #64 Feat: k6 테스트 스크립트 수정 / Grafana 대시보드 수정 / 입장 메시지 출력 로직 추가

--- chat-service/src/main/java/com/potatalk/config/StompHandler.java
+++ chat-service/src/main/java/com/potatalk/config/StompHandler.java
@@ -1,5 +1,9 @@
 package com.potatalk.config;
 
+import com.potatalk.pubsub.ChatEventProducer;
+import java.security.Principal;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
@@ -9,17 +13,26 @@ import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class StompHandler implements ChannelInterceptor {
 
+    private final ChatEventProducer chatEventProducer;
+
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        log.info("join preSend");
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
             // 클라이언트가 WebSocket을 통해 연결할 때 처리 로직
+            log.info("CONNECT 확인");
         } else if (StompCommand.SUBSCRIBE.equals(accessor.getCommand())) {
             // 클라이언트가 채팅방에 입장할 때 처리 로직
+            String destination = accessor.getDestination();
+            String roomId = extractRoomId(destination);
+            chatEventProducer.sendJoinEvent(roomId, "test-user");
+
         } else if (StompCommand.SEND.equals(accessor.getCommand())) {
             // 클라이언트가 메시지를 보낼 때 처리 로직
         } else if (StompCommand.UNSUBSCRIBE.equals(accessor.getCommand())) {
@@ -27,5 +40,12 @@ public class StompHandler implements ChannelInterceptor {
         }
 
         return ChannelInterceptor.super.preSend(message, channel);
+    }
+
+    private String extractRoomId(String destination) {
+        if (destination != null && destination.startsWith("/sub/chat/room/")) {
+            return destination.replace("/sub/chat/room/", "");
+        }
+        return "unknown-room";
     }
 }
--- chat-service/src/main/java/com/potatalk/pubsub/ChatEventProducer.java
+++ chat-service/src/main/java/com/potatalk/pubsub/ChatEventProducer.java
@@ -1,19 +1,27 @@
 package com.potatalk.pubsub;
 
+import com.potatalk.metric.WebSocketMetrics;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class ChatEventProducer {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final WebSocketMetrics webSocketMetrics;
 
-    public ChatEventProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    public ChatEventProducer(KafkaTemplate<String, String> kafkaTemplate, WebSocketMetrics webSocketMetrics) {
         this.kafkaTemplate = kafkaTemplate;
+        this.webSocketMetrics = webSocketMetrics;
     }
 
     public void sendJoinEvent(String roomId, String username) {
         String message = String.format("User %s has joined room %s", username, roomId);
-        kafkaTemplate.send("chat.join", roomId, message); // topic, key, value
+        log.info(message);
+        webSocketMetrics.recordMessage(() ->
+            kafkaTemplate.send("chat.join", roomId, message).join()
+        );
     }
 }
