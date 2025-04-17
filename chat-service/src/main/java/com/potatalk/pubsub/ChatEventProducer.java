3f170bef91f4fb779cfd6dd7ab4842152b5dac24
tangpoo <lhinav3@gmail.com>
2025-04-17 16:04:20 +0900

    #64 Feat: Prometheus에서 RabbitMQ 메트릭 수집 / RabbitMQConfig에서 테스트 전 Exchange 설정

--- chat-service/src/main/java/com/potatalk/pubsub/ChatEventProducer.java
+++ chat-service/src/main/java/com/potatalk/pubsub/ChatEventProducer.java
@@ -1,27 +1,25 @@
 package com.potatalk.pubsub;
 
 import com.potatalk.metric.WebSocketMetrics;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class ChatEventProducer {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final AmqpTemplate amqpTemplate;
     private final WebSocketMetrics webSocketMetrics;
-
-    public ChatEventProducer(KafkaTemplate<String, String> kafkaTemplate, WebSocketMetrics webSocketMetrics) {
-        this.kafkaTemplate = kafkaTemplate;
-        this.webSocketMetrics = webSocketMetrics;
-    }
 
     public void sendJoinEvent(String roomId, String username) {
         String message = String.format("User %s has joined room %s", username, roomId);
         log.info(message);
         webSocketMetrics.recordMessage(() ->
-            kafkaTemplate.send("chat.join", roomId, message).join()
+            amqpTemplate.convertAndSend("chat.join", roomId, message)
         );
     }
 }
--- chatRoom-service/src/main/java/com/potatalk/chatroomservice/service/ChatRoomService.java
+++ chatRoom-service/src/main/java/com/potatalk/chatroomservice/service/ChatRoomService.java
@@ -8,6 +8,7 @@ import com.potatalk.chatroomservice.dto.ChatRoomInfoRes;
 import com.potatalk.chatroomservice.dto.CreateChatRoomDto;
 import com.potatalk.chatroomservice.exception.ChatRoomNotFound;
 import com.potatalk.chatroomservice.exception.PrivateKeyIsNotMatchedException;
+import com.potatalk.chatroomservice.publisher.ChatRoomPublisher;
 import com.potatalk.chatroomservice.publisher.KafkaPublisherImpl;
 import com.potatalk.chatroomservice.repository.ChatRoomRepository;
 import com.potatalk.chatroomservice.repository.ParticipationRepository;
@@ -27,8 +28,8 @@ public class ChatRoomService {
     private final ChatRoomRepository chatRoomRepository;
     private final ParticipationRepository participationRepository;
     private final TransactionalOperator transactionalOperator;
-    //    private final ChatRoomPublisher chatRoomPublisher;
-    private final KafkaPublisherImpl chatRoomPublisher;
+    private final ChatRoomPublisher chatRoomPublisher;
+//    private final KafkaPublisherImpl chatRoomPublisher;
 
     public Mono<ChatRoom> createChatRoom(CreateChatRoomDto createChatRoomDto) {
         Long memberId = createChatRoomDto.getMemberId();
