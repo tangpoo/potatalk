c5710f8008a1142aa651338173c746e06fd2a5f8
tangpoo <lhinav3@gmail.com>
2025-06-23 18:13:18 +0900

    #64 Test: 메시지브로커 변경으로 인한 테스트 업데이트

---
+++ call-service/src/test/java/com/codingtest/callservice/CallServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingtest.callservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
